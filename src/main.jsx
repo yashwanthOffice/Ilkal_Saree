5621bd99f778f91d1bcdc99088217a1681c77f53
yash0123 <150314822+yashwanthOffice@users.noreply.github.com>
2026-09-09 19:37:31 +0530

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import { CatalogProvider } from './context/CatalogContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <CatalogProvider>
         <App />
       </CatalogProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 )
